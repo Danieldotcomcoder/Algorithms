e26645a8b6b62e84ee9473e6730ef390d7043607
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-04-04 20:51:37 +0300

    Add my max array leetcode solution

--- maxarray.js
+++ maxarray.js
@@ -1,23 +1,20 @@
 var maxArea = function (height) {
-    let maxarray = [];
-    let leftpointer = 0;
-    let rightpointer = height.length - 1;
-    while (leftpointer < height.length && rightpointer >= 0) {
-      if (height[leftpointer] <= height[rightpointer]) {
-        maxarray.push(Math.abs(leftpointer - rightpointer) * height[leftpointer]);
-        leftpointer++;
-      } else {
-        maxarray.push(
-          Math.abs(leftpointer - rightpointer) * height[rightpointer]
-          
-        );
-        rightpointer--;
-      }
-      
-      
+  let maxarray = [];
+  let leftpointer = 0;
+  let rightpointer = height.length - 1;
+  while (leftpointer < height.length && rightpointer >= 0) {
+    if (height[leftpointer] <= height[rightpointer]) {
+      maxarray.push(Math.abs(leftpointer - rightpointer) * height[leftpointer]);
+      leftpointer++;
+    } else {
+      maxarray.push(
+        Math.abs(leftpointer - rightpointer) * height[rightpointer]
+      );
+      rightpointer--;
     }
-  
-    return Math.max(...maxarray);
-  };
-  
-  maxArea(arr)
+  }
+
+  return Math.max(...maxarray);
+};
+
+maxArea(arr);
